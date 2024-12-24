8191af6b7ed4c5e2795dca1c1265fbfd43722105
RajDaBest <imagination12344321.com@gmail.com>
2024-12-24 15:03:48 +0530

    new allocator.c file

--- allocator/allocator.h
+++ allocator/allocator.h
@@ -2,7 +2,6 @@
 #define B4E8B570_191E_40E7_BFB3_10EB1EAE4545
 
 #include <stdlib.h>
-
 #include "mem_alloc.h"
 
 typedef void *(*allocator_t)(size_t size);
@@ -16,52 +15,7 @@ void default_deallocator(void *ptr);
 void *custom_allocator(size_t size);
 void custom_deallocator(void *ptr);
 
-#define CUSTOM_ALLOCATOR
-
-allocator_t allocate =
-#ifndef CUSTOM_ALLOCATOR
-    default_allocator;
-#else
-    custom_allocator;
-#endif
-
-deallocator_t deallocate =
-#ifndef CUSTOM_ALLOCATOR
-    default_deallocator;
-#else
-    custom_deallocator;
-#endif
-
-void *default_allocator(size_t size)
-{
-    return malloc(size);
-}
-
-void default_deallocator(void *ptr)
-{
-    free(ptr);
-}
-
-void *custom_allocator(size_t size)
-{
-    return heap_alloc(size, ALIGN_DEFAULT);
-}
-
-void custom_deallocator(void *ptr)
-{
-    heap_free(ptr);
-}
-
-void change_allocator_to_default()
-{
-    allocate = default_allocator;
-    deallocate = default_deallocator;
-}
-
-void change_allocator_to_custom()
-{
-    allocate = custom_allocator;
-    deallocate = custom_deallocator;
-}
+extern allocator_t allocate;
+extern deallocator_t deallocate;
 
 #endif /* B4E8B570_191E_40E7_BFB3_10EB1EAE4545 */
